9b4cd7a0741cc7ffcaf838b5d53426852c64dde5
Elbane3 <120138927+Elbane3@users.noreply.github.com>
2023-06-15 13:01:11 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,59 +1,46 @@
 import socket
-from _thread import *
-import sys
+from essai_31 import Connection
+from essai_31 import initialize_game_state
+from essai_31 import update_game_state
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connections = []
+    
+    def start(self):
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen()
+        print(f"Server listening on {self.host}:{self.port}")
+        while True:
+            client_socket, address = self.server_socket.accept()
+            print(f"New connection from {address}")
+            connection = Connection(client_socket)
+            self.connections.append(connection)
+            self.handle_client(connection)
+    
+    def handle_client(self, connection):
+        # Initialize the game state
+        game_state = initialize_game_state()
 
-server = '127.0.0.1'
-port = 5555
+        while True:
+            try:
+                # Receive the client's move and update the game state
+                move = connection.receive_move()
+                game_state = update_game_state(game_state, move)
 
-server_ip = socket.gethostbyname(server)
+                # Send the updated game state to all clients
+                for other_connection in self.connections:
+                    if other_connection != connection:
+                        other_connection.send_game_state(game_state)
 
-
-try:
-    s.bind((server, port))
-
-except socket.error as e:
-    print(str(e))
-
-print("Waiting for a connection")
-
-currentId = "0"
-pos = ["0:50,50", "1:100,100"]
-def threaded_client(conn):
-    global currentId, pos
-    conn.send(str.encode(currentId))
-    currentId = "1"
-    reply = ''
-    while True:
-        try:
-            data = conn.recv(2048)
-            reply = data.decode('utf-8')
-            if not data:
-                conn.send(str.encode("Goodbye"))
+            except:
+                print(f"Client disconnected")
+                self.connections.remove(connection)
+                connection.close()
                 break
-            else:
-                print("Recieved: " + reply)
-                arr = reply.split(":")
-                id = int(arr[0])
-                pos[id] = reply
-
-                if id == 0: nid = 1
-                if id == 1: nid = 0
-
-                reply = pos[nid][:]
-                print("Sending: " + reply)
-
-            conn.sendall(str.encode(reply))
-        except:
-            break
-
-    print("Connection Closed")
-    conn.close()
-
-while True:
-    s.listen()
-    conn, addr = s.accept()
-    print("Connected to: ", addr)
-
-    start_new_thread(threaded_client, (conn,))
+   
+server = Server('10.55.123.179',8888)
+server.start()
